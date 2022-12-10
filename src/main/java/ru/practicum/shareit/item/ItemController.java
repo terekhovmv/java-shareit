bc14a581588de42eef4a086fd9aebfd3314707ba
Max.Terekhov <terekhovmv@yandex.ru>
2022-12-10 13:49:19 +0300

    plan: initial sprint TODOs

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO Sprint add-controllers.
+ * POST /items
+ * PATCH /items/:itemId
+ * GET /items/:itemId
+ * GET /items
+ * GET /items/search?text=
  */
 @RestController
 @RequestMapping("/items")
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO Sprint add-controllers.
+ * POST /users
+ * PATCH /users/:userId
+ * GET /users/:userId
+ * DEL /users/:userId
+ * GET /users
  */
 @RestController
 @RequestMapping(path = "/users")
