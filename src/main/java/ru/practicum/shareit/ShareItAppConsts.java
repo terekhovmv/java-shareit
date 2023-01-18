a87d70632174bbd083f35f0f82b931c0a7332cfc
Max.Terekhov <terekhovmv@yandex.ru>
2023-01-18 20:57:31 +0300

    refactor: removes useless const

--- src/main/java/ru/practicum/shareit/ShareItAppConsts.java
+++ src/main/java/ru/practicum/shareit/ShareItAppConsts.java
@@ -2,5 +2,4 @@ package ru.practicum.shareit;
 
 public final class ShareItAppConsts {
     public static final String HEADER_CALLER_ID = "X-Sharer-User-Id";
-    public static final int DEFAULT_PAGE_SIZE = 20;
 }
