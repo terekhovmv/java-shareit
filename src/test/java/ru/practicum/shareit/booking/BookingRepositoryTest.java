92589dec31acffeb95b86b4ac8c8c4d7136f3fc9
Max.Terekhov <terekhovmv@yandex.ru>
2023-01-23 07:03:41 +0300

    test: adds tests for non trivial repository methods

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -10,6 +10,8 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.practicum.shareit.booking.BookingAssertions.assertBookingNotFound;
 
@@ -52,4 +54,49 @@ public class BookingRepositoryTest {
                 () -> testee.require(Long.MAX_VALUE)
         );
     }
+
+    @Test
+    void getFinishedCount() {
+        User owner = userRepository.save(
+                new User(null, "owner", "owner@abc.def")
+        );
+        User booker = userRepository.save(
+                new User(null, "booker", "booker@abc.def")
+        );
+        User otherBooker = userRepository.save(
+                new User(null, "other-booker", "other-booker@abc.def")
+        );
+        Item item = itemRepository.save(
+                new Item(null, "item", "item-description", true, owner, null)
+        );
+        Item otherItem = itemRepository.save(
+                new Item(null, "other-item", "other-item-description", true, owner, null)
+        );
+
+        addBooking(-1, item, booker, BookingStatus.APPROVED); //suitable
+        addBooking(-1, item, otherBooker, BookingStatus.APPROVED); //booker mismatch
+        addBooking(1, item, otherBooker, BookingStatus.APPROVED); //end date mismatch
+        addBooking(-1, item, booker, BookingStatus.REJECTED); //status mismath
+        addBooking(-1, otherItem, booker, BookingStatus.APPROVED); //item mismatch
+        addBooking(-2, item, booker, BookingStatus.APPROVED); //suitable
+
+        assertEquals(
+                2,
+                testee.getFinishedCount(booker.getId(), item.getId(), LocalDateTime.now())
+        );
+    }
+
+    private Booking addBooking(int endDays, Item item, User booker, BookingStatus status) {
+        return testee.save(
+                new Booking(
+                        null,
+                        LocalDateTime.now().plusDays(endDays - 1000),
+                        LocalDateTime.now().plusDays(endDays),
+                        item,
+                        booker,
+                        status
+                )
+        );
+    }
+
 }
--- src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -3,11 +3,16 @@ package ru.practicum.shareit.item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.pagination.RandomAccessPageRequest;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static ru.practicum.shareit.item.ItemAssertions.assertItemNotFound;
 
 @DataJpaTest
@@ -38,6 +43,45 @@ public class ItemRepositoryTest {
         assertItemNotFound(
                 Long.MAX_VALUE,
                 () -> testee.require(Long.MAX_VALUE)
+        );
+    }
+
+    @Test
+    void getAllAvailableWithText() {
+        User owner = userRepository.save(
+                new User(null, "name", "name@abc.def")
+        );
+        Item album = addItem(owner, "album_ABC");
+        addItem(owner, "banjo");
+        Item capri = addItem(owner, "capri_abc");
+        Item dwarf = addItem(owner, "ABC_dwarf");
+        addItem(owner, "ebook");
+        Item fader = addItem(owner, "abc_fader");
+        Item groot = addItem(owner, "groot_ABC_groot");
+        Item heels = addItem(owner, "HEELS_abc_HEELS");
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+
+        assertIterableEquals(
+                testee.getAllAvailableWithText(
+                        "abc",
+                        RandomAccessPageRequest.of(0, 100, sort)
+                ),
+                List.of(heels, groot, fader, dwarf, capri, album)
+        );
+
+        assertIterableEquals(
+                testee.getAllAvailableWithText(
+                        "ABC",
+                        RandomAccessPageRequest.of(1, 4, sort)
+                ),
+                List.of(groot, fader, dwarf, capri)
+        );
+    }
+
+    private Item addItem(User owner, String name) {
+        return testee.save(
+                new Item(null, name, name + "description", true, owner, null)
         );
     }
 }
