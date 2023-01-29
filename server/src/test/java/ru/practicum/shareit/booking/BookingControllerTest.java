0a1ad11370023df6cf0aa6c3f70407e6d3823ef5
Max.Terekhov <terekhovmv@yandex.ru>
2023-01-29 18:32:28 +0300

    refactor: removes unused import

--- server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
