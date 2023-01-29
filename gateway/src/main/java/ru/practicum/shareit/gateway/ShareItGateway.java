89af8942ea89c18e8943bf6c3bbc8a38f02b223a
Max.Terekhov <terekhovmv@yandex.ru>
2023-01-29 12:18:31 +0300

    refactor: dedicated gateway package to prevent name duplication

--- gateway/src/main/java/ru/practicum/shareit/gateway/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/ShareItGateway.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit;
+package ru.practicum.shareit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gateway/src/main/java/ru/practicum/shareit/gateway/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/client/BaseClient.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.client;
+package ru.practicum.shareit.gateway.client;
 
 import java.util.List;
 import java.util.Map;
