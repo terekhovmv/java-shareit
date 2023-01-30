25e11a62b76368233b09070f0af38f41324a10a1
Max.Terekhov <terekhovmv@yandex.ru>
2023-01-30 07:08:25 +0300

    refactor: adds using HEADER_CALLER_ID from common module in BaseClient

--- gateway/src/main/java/ru/practicum/shareit/gateway/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/client/BaseClient.java
@@ -4,6 +4,7 @@ import org.springframework.http.*;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import ru.practicum.shareit.ShareItAppConsts;
 
 import java.util.List;
 import java.util.Map;
@@ -110,7 +111,7 @@ public class BaseClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         if (userId != null) {
-            headers.set("X-Sharer-User-Id", String.valueOf(userId));
+            headers.set(ShareItAppConsts.HEADER_CALLER_ID, String.valueOf(userId));
         }
         return headers;
     }
